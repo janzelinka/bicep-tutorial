04705439f705352051d2f24543540db1c5265bd4
Jan Zelinka <jan.zelinka12@gmail.com>
2025-07-20 18:07:56 +0200

    removed comment

--- script.ps1
+++ script.ps1
@@ -1,5 +1,1 @@
-# if running first time
-#in powershell
-#winget install -e --id Microsoft.Bicep
-
 New-AzResourceGroupDeployment -ResourceGroupName lesson02 -TemplateFile main.bicep -Name powershell-deployment
